2b2dcda4eb70ab1eeddb7fb773bc1112770da550
Raj Kumar <raj9798kumar6134@gmail.com>
2025-11-07 08:03:37 +0530

    Port switched to 3002

--- app.py
+++ app.py
@@ -793,4 +793,4 @@ def health_check():
 # ------------------------------
 if __name__ == '__main__':
     logger.info("Starting merged Speech+Face Emotion server...")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=3002)
